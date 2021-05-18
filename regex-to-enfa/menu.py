6078edefc9f0c698d27af5049a7b403a79e5cfa6
Matheus Reis <matheusjosereis99@gmail.com>
2021-05-17 21:27:37 -0300

    Adding regex-to-enfa files

---
+++ regex-to-enfa/menu.py
@@ -0,0 +1,53 @@
+import afne
+from er_para_afne import *
+
+def menu() -> None:
+  afne_nome: str = "./afne_convertido.json"
+  afne_exemplo: str = "exemplo_1"
+  robocop: afne = afne.AFNE(afne_nome, afne_exemplo, True)
+
+  print("Selecione a opção: ")
+  print("1) AFNE")
+  print("2) Conversor ER -> AFNE")
+  print("3) Sair")
+
+  opcao_1: int = int(input("Opcao: "))
+
+  while True:
+    if opcao_1 == 3:
+      break
+    elif opcao_1 == 2:
+      automato: dict = {}
+      expressao = str(input("Digite uma expressão (Digite 'sair' para sair): "))
+
+      if expressao == 'sair':
+        break
+      automato = regex_to_afne(expressao)
+      printa_automato(automato)
+
+      print('*'*50)
+      opcao_2 = str(input("Escrever no arquivo? [Y/N]: ")).lower()
+      print('*'*50,'\n')
+
+      if opcao_2 == 'y':
+        escreve_no_arquivo(automato, expressao)
+        print("Saindo :(\n")  
+        return                      
+              
+    elif opcao_1 == 1:
+      robocop.show()
+      while (True):        
+        print('\n\n')
+        entrada: str = input("Digite uma entrada (separe os simbolos por espaços ou digite 'sair'): ")
+
+        if entrada.lower() == 'sair':
+          break
+        if robocop.verifica_cadeia(entrada):
+          print("\033[32mCADEIA ACEITA!!!\033[0;0m\n")
+        else:
+          print("\033[31mCADEIA REJEITADA!!!\033[0;0m\n")
+        print("="*128)
+      print("Saindo . . .\n")  
+      return
+
+menu()
